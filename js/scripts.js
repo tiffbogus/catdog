8d8e498927650af01ede32c2b76f65199bd4c1e6
Tiffany Bogus & Arden Thira <student@epicodus.com>
2015-10-05 19:18:30 -0700

    Add column for human responses; add gifs and scripts for toggle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
+  $("#hiss").click(function(){
+    $("#hiss-showing").toggle();
+    $("#hiss-hidden").toggle();
+  });
+
   $("button#meow").click(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#dog").prepend("<li>Bark!</li>");
+    $("ul#stop").prepend("<li>No wet food for you tonight!</li>");
   });
-
 
 
   $("button#bark").click(function() {
     $("ul#cat").prepend("<li>Hiss!</li>");
     $("ul#dog").prepend("<li>Woof!</li>");
+    $("ul#stop").prepend("<li>Dangit, Ranger!</li>");
   });
 
-
+  $("button#stop").click(function() {
+    $("ul#cat").prepend("<li>::ignores::</li>");
+    $("ul#dog").prepend("<li>::whimpers::</li>");
+    $("ul#stop").prepend("<li>Oh all right. Come snuggle with me.</li>");
+  });
 
 
 });
