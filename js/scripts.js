0344a6ec94bf775c4cbd1dfcb1d4e51e2b676359
Tiffany Bogus & Arden Thira <student@epicodus.com>
2015-10-05 18:45:34 -0700

    edit catdog js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("button#meow").click(function() {
+    $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#dog").prepend("<li>Bark!</li>");
+  });
+
+
+
+  $("button#bark").click(function() {
+    $("ul#cat").prepend("<li>Hiss!</li>");
+    $("ul#dog").prepend("<li>Woof!</li>");
+  });
+
+
+
+
+});
